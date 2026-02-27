33ac91dd5b0a1d723897cfcfce7e1adbe6eb51cc
James Tlaloc <jamestlaloc.dev@gmail.com>
2026-02-26 17:25:23 -0800

    Grab section tag and create a paraggraph tag.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const link = document.querySelector("a");
 link.textContent = "Mozilla Developer Network";
 link.href = "https://developer.mozilla.org";
+
+const section = querySelector("section");
+
+const para = document.createElement("p");
+para.textContent = "We hope you enjoyed the ride.";
