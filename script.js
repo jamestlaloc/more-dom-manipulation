79109c864befba1b9c176b15782aa1ffe9b68d08
James Tlaloc <jamestlaloc.dev@gmail.com>
2026-03-06 23:26:57 -0800

    Add to the page content.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ link.href = "https://developer.mozilla.org";
 const sect = document.querySelector("section");
 
 const para = document.createElement("p");
-para.textContent = "We hope you enjoyed the ride.";
+para.textContent = "We hope you enjoyed the ride, athough it was short.";
 
 const text = document.createTextNode( " - the premier source for web development knowledge.")
 
