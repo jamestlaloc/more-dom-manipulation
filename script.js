8556e503e6ecd1e3f4fe9bb0cc26f6edd307c4a7
James Tlaloc <jamestlaloc.dev@gmail.com>
2026-03-06 23:28:24 -0800

    And more.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const link = document.querySelector("a");
-link.textContent = "Mozilla Developer Network";
+link.textContent = "Mozilla Developer Network (Not Godzilla)";
 link.href = "https://developer.mozilla.org";
 
 const sect = document.querySelector("section");
